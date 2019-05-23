b0296e6853b0b47d15c66b47495d69098285728d
Denali Balser <denali.balser167@gmail.com>
2019-05-23 20:41:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,9 +7,12 @@ class Dog
   end
 
   def self.all
-    @@all.each do |name|
-      puts name
-    end 
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
 
+  def self.clear_all
+    @@all.clear
+  end
 end
